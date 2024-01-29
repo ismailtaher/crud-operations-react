8b1565a544b110b15bc45129af01185a82bae2d5
ismailtaher <ismail.taher@ymail.com>
2024-01-29 20:00:53 +0500

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState, useEffect } from "react";
+import apiRequest from "./apiRequest";
 
 function App() {
   const API_URL = "http://localhost:3500/items";
@@ -24,6 +25,7 @@ function App() {
         const response = await fetch(API_URL);
         if (!response.ok) throw Error("Did not receive expected data");
         const listItems = await response.json();
+        console.log(listItems);
         setItems(listItems);
         setFetchError(null);
       } catch (err) {
@@ -39,18 +41,42 @@ function App() {
     }, 2000);
   }, []);
 
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const addItem = async (item) => {
+    const idNum = items.length ? Number(items[items.length - 1].id) + 1 : 1;
+    const id = idNum.toString();
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myNewItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json-patch",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    console.log(id);
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
   const handleDelete = (id) => {
